430afc81d528b84e20f4aff5235415c9ee2bf175
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-04 13:03:32 -0400

    Created component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
 
-const InterviewerList = () => {
+// const interviewers = [
+//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
+// ];
 
-  return (
-    <p>I AM INTERVIEWER LIST</p>
-  )
-}
+const InterviewerList = props => {
 
-export default InterviewerList
+  const loopInterviewerList = props.interviewers.map(interviewer => {
+    return (
+      <section className="interviewers">
+        <h4 className="interviewers__header text--light">
+          {interviewer.name}</h4>
+        <ul className="interviewers__list"></ul>
+      </section>
+    );
+  })
+  return loopInterviewerList;  
+};
+
+export default InterviewerList;
