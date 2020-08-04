7482eab9fbeb436b03103951859e9b30a4314f67
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-04 15:07:46 -0400

    Imported component + added story

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -138,4 +139,10 @@ storiesOf("InterviewerList", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show 
+    student="Lydia Miller-Jones"
+    interviewer={3}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+    />)
