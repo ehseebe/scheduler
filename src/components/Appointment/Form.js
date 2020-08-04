a4e6b267eaa85f44c83a4c66651da9e0bd273f26
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-04 17:35:18 -0400

    Implement component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
+
 const Form = props => {
+  const [name, setName] = useState(props.name || '');
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+      setName('');
+      setInterviewer(null);
+      // props.onSave();
+  }
+
+  const cancel = () => {
+    reset()
+    props.onCancel();
+  }
+
+  // const myChangeHandler = (event) => {
+  //   this.setName({name: event.target.value})
+  // }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
+            name={name}
             type="text"
             placeholder="Enter Student Name"
-            /*
-          This must be a controlled component
-        */
+            onChange={props.setName}
           />
         </form>
         <InterviewerList
           interviewers={props.interviewers}
-          value={props.interviewer}
+          value={interviewer}
           onChange={props.setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
-          <Button confirm>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={reset}>Save</Button>
         </section>
       </section>
     </main>
