00fb3991df84f87795a62a056bacad014233bc96
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-04 15:26:56 -0400

    Added stories for Status and Error

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,8 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -151,4 +153,9 @@ storiesOf("InterviewerList", module)
     message="Delete the appointment?"
     onConfirm={action("onConfirm")}
     onCancel={action("onCancel")}
+    />)
+  .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => <Error 
+    message="Could not delete appointment."
+    onClose={action("onClose")}
     />)
