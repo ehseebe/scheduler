28c8a79d1dd1f32fb100cbd68982172fe3fe6b59
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-04 12:26:20 -0400

    File creation

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import InterviewerListItem from "./InterviewerListItem";
+
+const InterviewerList = () => {
+
+  return (
+    <p>I AM INTERVIEWER LIST</p>
+  )
+}
+
+export default InterviewerList
