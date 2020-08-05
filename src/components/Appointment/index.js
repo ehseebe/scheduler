9eeaa8c3c180706c8d0eabbc64e75e5585677160
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-04 20:09:05 -0400

    Added header and conditionals

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
-const Appointment = () => {
+
+const Appointment = props => {
   return(
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? <Show interviewer={props.interview.interviewer} student={props.interview.student} /> : <Empty />}
+    </article>
   )
 }
 
