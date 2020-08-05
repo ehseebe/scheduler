48b62dfca3a2c747ab6fb5f61c57dbfe173746d2
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-05 16:02:08 -0400

    Setting up days to display
    Co-authored-by: Karl Chvojka <karl.chvojka@protonmail.com>

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
 import "components/Appointment";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+const axios = require('axios').default;
 
 const interviewers = [
   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
@@ -78,13 +61,26 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState([]);
   const [interviewer, setInterviewer] = useState("Sylvia Palmer");
   //const [appointment, setAppointment] = useState(appointments);
 
   // const addNewAppointments = (appointment) => {
   //   setAppointment(prevState => [...prevState, appointment])
   // }
+
+  useEffect(() => {
+    axios.get('api/days')
+    .then((days) => {
+      console.log(days.data);
+      setDay(days.data);
+    })
+    .catch((error) => { 
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      console.log(error.response.data);
+    })
+  }, []);
 
   return (
     <main className="layout">
@@ -97,7 +93,7 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
+            days={day}
             day={day}
             setDay={setDay}
           />
