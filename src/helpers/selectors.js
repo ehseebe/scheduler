23dd533233aa419bf7ec761dc8c8b8ee09ed54d1
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-06 08:25:59 -0400

    removed comments

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,4 @@
 export function getAppointmentsForDay(state, name) {
-  //iterate through filteredDays
-  //compare where appointments.id === days.appointments
 
   const foundDay = state.days
     .filter((day) => day.name === name)[0];
