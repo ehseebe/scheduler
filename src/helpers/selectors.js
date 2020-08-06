8399fa71834c4ed2506ae7de4582be9635ac1d18
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-06 14:31:20 -0400

    testing getInterview

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,20 +1,31 @@
 export function getAppointmentsForDay(state, name) {
+  const foundDay = state.days.filter((day) => day.name === name)[0];
 
-  const foundDay = state.days
-    .filter((day) => day.name === name)[0];
-  
-    console.log("???", foundDay)
+  //console.log("???", foundDay)
 
   const output = foundDay
-    ? foundDay.appointments
-      .map((id) => state.appointments[id])
+    ? foundDay.appointments.map((id) => state.appointments[id])
     : [];
-  
-    console.log("OUTPUT", output)
 
-  return output
+  //console.log("OUTPUT", output)
+
+  return output;
 }
 
 export function getInterview(state, interview) {
+  
+  if (!interview){
+    return null;
+  }
+
+  //needs to return object with id, name, avatar
+  const interviewerId = interview.interviewer
+  console.log("!!!!!!", interviewerId)
+  const output = {
+    student: interview.student,
+    interviewer: state.interviewers
+  }
+  console.log("???", output)
+  return output
 
 }
