776187a7faeecd6405a444b0a62bf8a960d650c5
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-06 13:57:16 -0400

    Console logs for verification

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,9 +2,19 @@ export function getAppointmentsForDay(state, name) {
 
   const foundDay = state.days
     .filter((day) => day.name === name)[0];
+  
+    console.log("???", foundDay)
 
-  return foundDay
+  const output = foundDay
     ? foundDay.appointments
       .map((id) => state.appointments[id])
     : [];
+  
+    console.log("OUTPUT", output)
+
+  return output
+}
+
+export function getInterview(state, interview) {
+
 }
