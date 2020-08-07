709b397baf1bb6654d517bd821c4849f5c2e537e
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-07 15:15:11 -0400

    Implemented save

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,37 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
+import Confirm from "./Confirm";
 import useVisualMode from "hooks/useVisualMode";
 
 const Appointment = (props) => {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const EDIT = "EDIT";
+  const CONFIRM = "CONFIRM";
+  const SAVING = "SAVING";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+
+    // console.log(props.id, interview)
+    transition(SAVING)
+    props.bookInterview(props.id, interview)
+    .then(() => transition(SHOW));
+  }
+
+  function deleteAppt(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+
+    // console.log(props.id, interview)
+    transition(SAVING)
+    props.cancelInterview(props.id, interview)
+    .then(() => transition(EMPTY));
+  }
 
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === CREATE && (
-        <Form 
-          interviewers={[]}
-          // onSave={onSave}
+        <Form
+          interviewers={props.interviewers}
+          onSave={save}
           onCancel={() => back()}
-          />
+        />
       )}
       {mode === SHOW && (
         <Show
-          interviewer={props.interview.interviewer}
           student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onEdit={() => transition(EDIT)}
+          onDelete={() => transition(CONFIRM)}
         />
       )}
+      {mode === SAVING && <Status message='Saving' />}
+      {mode === SAVING && <Status message='Deleting' />}
     </article>
   );
 };
