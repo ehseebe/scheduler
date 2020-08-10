4d052b0536631f15b5725bfed004bc20fc28d6a1
Alyssa Beaton Tanguay <alyssa@macbook.local>
2020-08-10 15:42:46 -0400

    Finished mocking gets

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -64,11 +64,19 @@ export default {
     }
 
     if (url === "/api/appointments") {
-      /* Resolve appointments data */
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments
+      });
     }
 
     if (url === "/api/interviewers") {
-      /* Resolve interviewers data */
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.interviewers
+      });
     }
   })
 }
